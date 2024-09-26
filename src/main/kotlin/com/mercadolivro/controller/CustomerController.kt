6417cf14d35c48a0f03632985de5ac0155795ff5
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2024-09-26 14:00:05 -0300

    change path in the endpoint

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,12 +1,19 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
     @GetMapping
     fun helloWorld(): String {
-        return "Hello World"
+        return "Customer 1"
+    }
+
+    @GetMapping("/2")
+    fun customer2(): String {
+        return "Customer 2"
     }
 }
