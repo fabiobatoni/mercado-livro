b55f82fb29a9d38edaa6f9d372dd6c6d1f456d19
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2024-09-27 10:42:45 -0300

    implements POST customer

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +14,10 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Fabio", "fabio_teste@hotmail.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 }
