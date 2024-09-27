6fd306a76cbb54f5ad760625d28b436f63c67c8b
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2024-09-27 10:31:46 -0300

    create customerModel for return Customer in GET request

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,19 +1,15 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customers")
 class CustomerController {
     @GetMapping
-    fun helloWorld(): String {
-        return "Customer 1"
-    }
-
-    @GetMapping("/2")
-    fun customer2(): String {
-        return "Customer 2"
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Fabio", "fabio_teste@hotmail.com")
     }
 }
---
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,7 @@
+package com.mercadolivro.model
+
+data class CustomerModel (
+    var id: String,
+    var name: String,
+    var email: String
+)
