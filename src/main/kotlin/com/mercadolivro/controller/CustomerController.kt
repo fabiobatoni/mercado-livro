c99a1fb1ba25d1ef00c3a014c54cb72394a102bd
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2024-09-27 12:45:56 -0300

    refactoring code

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -13,14 +13,24 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("customers")
 class CustomerController {
+
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Fabio", "fabio_teste@hotmail.com")
+    fun getCustomer(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+
+        val id = if(customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
 }
